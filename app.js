758c817282b0e31f192d78494504a67520fe7bd1
Klisman Salvoporcristo <108096965+klismansalvoporcristo@users.noreply.github.com>
2025-05-31 12:47:13 -0300

    ATUALIZAÇÃO 1.2

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ function calcular(){
     var umidade = document.getElementById("umidade").value;
     var anidro = document.getElementById("anidro").value;
     var ddgs = document.getElementById("ddgs").value;
+    var mosto = document.getElementById("mosto").value;
+    var vinho = document.getElementById("vinho").value;
 
     var moagemcor = ((100 - umidade)/86)*moagem
     var rendetanol = (anidro/moagemcor)*1000
-    var rendddgs = (ddgs/moagemcor)*1000
+    var rendddgs = ddgs/(vinho/(mosto/moagemcor))*1000
 
     document.getElementById("rendimentoetn").innerText="RENDIMENTO DO ETANOL : "+rendetanol.toFixed(3)
     document.getElementById("rendimentodd").innerText="RENDIMENTO DO DDGS : "+rendddgs.toFixed(3) 
